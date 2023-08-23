8d68d3d7ba920fb7e3ff8d7cfea0006632425570
Nadia Ou Jiaqi <ovo.nadia@outlook.com>
2023-08-23 16:29:31 +0200

    finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :phone_number, presence: true
+  validates :category, inclusion: {in: %w(japanese french belgian chinese italian),
+      message: "%{value} is not a valid size" }
 end
